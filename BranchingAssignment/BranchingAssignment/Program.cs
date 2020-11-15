2c60ff8cb814680ed109f50d946f3e967d48cd66
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-11-15 14:28:45 -0700

    Branching Assignment

---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter package weight: ");
+            int packWeight = Convert.ToInt32(Console.ReadLine());
+
+            if ( packWeight <= 50)
+            {
+                Console.WriteLine("Please enter package width: ");
+                int packWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter package height: ");
+                int packHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter package length: ");
+                int packLength = Convert.ToInt32(Console.ReadLine());
+
+                int dimtotal = packLength + packHeight + packWidth;
+
+                if (dimtotal <= 50)
+                {
+                    double cost = (packLength * packHeight * packWidth * packWeight) / 100.00;
+                    Console.WriteLine("Your estimated total for shipping this package is: " + cost.ToString("C"));
+                    Console.WriteLine("Thank you!");
+                }
+                else
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- testWork/testWork/Program.cs
+++ testWork/testWork/Program.cs
@@ -11,23 +11,28 @@ namespace testWork
         static void Main(string[] args)
         {
 
+            //int num1 = 10;
+            //int num2 = 2;
+            //int addition = num1 + num2;
+            //int subtract = num1 - num2;
+            //int mult = num1 * num2;
+            //int div = num1 / num2;
+
+            //string nameF = "Kurt";
+            //string nameL = "Cutshall";
+
+
+
+            //Console.WriteLine(nameF + nameL);
+            //Console.WriteLine(addition);
+            //Console.WriteLine(subtract);
+            //Console.WriteLine(mult);
+            //Console.WriteLine(div);
+
             int num1 = 10;
             int num2 = 2;
-            int addition = num1 + num2;
-            int subtract = num1 - num2;
-            int mult = num1 * num2;
-            int div = num1 / num2;
-
-            string nameF = "Kurt";
-            string nameL = "Cutshall";
-
-
-
-            Console.WriteLine(nameF + nameL);
-            Console.WriteLine(addition);
-            Console.WriteLine(subtract);
-            Console.WriteLine(mult);
-            Console.WriteLine(div);
+            string result = num1 > num2 ? "num1 is greater than num2" : "num2 is greater than num1";
+            Console.WriteLine(result);
 
             Console.ReadLine();
         }
