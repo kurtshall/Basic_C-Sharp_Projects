ac90239d43009c83caaf89842c0d376cbb30a2e6
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-11-15 13:31:42 -0700

    Boolean Assignment

---
+++ BooleanAssignment/BooleanAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (enter \"true\" or \"false\")");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Are you approved for car inssuence?");
+            Console.WriteLine(age > 15 && DUI == false && tickets <= 3);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
