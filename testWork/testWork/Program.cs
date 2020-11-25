8137799d046e5d936501608af04a779c7e4b63b5
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-11-25 12:17:58 -0700

    Updated

--- testWork/testWork/Program.cs
+++ testWork/testWork/Program.cs
@@ -11,6 +11,34 @@ namespace testWork
         static void Main(string[] args)
         {
 
+            int day = 2;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Its the 1st day");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Its the 2nd day");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Its the 3rd day");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Its the 4th day");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Its the 5th day");
+                    break;
+            }
+
+
+            Console.ReadLine();
+
+
             //int num1 = 10;
             //int num2 = 2;
             //int addition = num1 + num2;
@@ -29,12 +57,12 @@ namespace testWork
             //Console.WriteLine(mult);
             //Console.WriteLine(div);
 
-            int num1 = 10;
-            int num2 = 2;
-            string result = num1 > num2 ? "num1 is greater than num2" : "num2 is greater than num1";
-            Console.WriteLine(result);
+            //int num1 = 10;
+            //int num2 = 2;
+            //string result = num1 > num2 ? "num1 is greater than num2" : "num2 is greater than num1";
+            //Console.WriteLine(result);
 
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
