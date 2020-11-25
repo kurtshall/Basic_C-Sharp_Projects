497ff97ee69d3b23e2d95e6566344093cf384198
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-11-25 12:47:11 -0700

    String Assignment

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string fName = "Kurt";
+            string lName = "Cutshall";
+            string color = "green";
+
+            Console.WriteLine(fName + " " + lName + "\'s favorite color is " + color + "!");
+            Console.WriteLine(color.ToUpper());
+
+            StringBuilder sb = new StringBuilder("Hello World!");
+            sb.Append(" What a nice day it is outside!");
+            sb.Append("\nWe should go for a walk later today.");
+            sb.Append("\nI think tomorrow will be another great day!");
+
+            Console.WriteLine(sb);
+
+
+            Console.ReadLine();
+        }
+    }
+}
