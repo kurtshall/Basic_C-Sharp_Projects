4f5fa6b40f6903752e8774b952916e5b6ffaea71
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-11-28 18:19:12 -0700

    6PartAssignemnt

---
+++ 6PartAssignment/6PartAssignment/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        List<string> colors = new List<string>();
+        colors.Add("green");
+        colors.Add("red");
+        colors.Add("blue");
+        colors.Add("yellow");
+        colors.Add("orange");
+
+        Console.WriteLine("Input a color: ");
+        colors.Add(Console.ReadLine());
+
+        for (int i = 1; i < colors.Count; i++)
+        {
+            Console.WriteLine(colors[i]);
+        }
+        
+
+        List<int> num = new List<int>();
+        num.Add(1);
+        num.Add(3);
+
+        for (int j = 0; j < 10; j++ )
+        {
+            num.Add(j);
+            Console.WriteLine(num[j]);
+        }
+        
+
+
+        List<int> num2 = new List<int>();
+        num.Add(4);
+        num.Add(5);
+
+        for (int j = 0; j <= 10; j++)
+        {
+            num.Add(j);
+            Console.WriteLine(num[j+3]);
+        }
+        
+
+        string[] names = { "Steve", "John", "Sarah", "Mike" };
+        Console.WriteLine("Type in a name to search: ");
+        string namecheck = (Console.ReadLine());
+
+
+        
+        for (int k = 0; k < names.Length; k++)
+        {
+            if (namecheck == names[k])
+            {
+                Console.WriteLine("That name was in the system!");
+            }
+
+            else if (k == names.Length - 1)
+            {
+                Console.WriteLine("That name was not in the system!");
+            }
+        }
+
+
+        string[] names2 = { "Steve", "John", "Sarah", "Mike", "John", "Sarah", "Joe" };
+        List<int> sameName = new List<int>();
+        Console.WriteLine("Type in a name to search how many there are: ");
+        string namecheck2 = (Console.ReadLine());
+
+
+        foreach (string pass in names2)
+        {
+            if (pass == namecheck2)
+            {
+                Console.WriteLine("That name was in the system!");
+                sameName.Add(1);
+            }
+                    
+            
+        }
+
+        Console.WriteLine(sameName.Count);
+
+        if (sameName.Count == 0)
+        {
+            Console.WriteLine("That name was not in the system!");
+        }
+
+
+
+        Console.ReadLine();
+    }
+}
+
---
+++ IterationTest/IterationTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+        //for (int i = 0; i < testScores.Length; i++)
+        //{
+        //    if (testScores[i] > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + testScores[i]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //    if (names[j] == "Jesse")
+        //    {
+        //        Console.WriteLine(names[j]);
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //List<int> testScores = new List<int>();
+        //testScores.Add(98);
+        //testScores.Add(99);
+        //testScores.Add(81);
+        //testScores.Add(72);
+        //testScores.Add(70);
+
+        //foreach (int score in testScores)
+        //{
+        //    if (score > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + score);
+        //    }
+        //}
+        //Console.ReadLine();
+
+
+        List<string> names = new List<string>() { "Jesse", "Erik", "Daniel", "Adam" };
+
+        foreach (string name in names)
+        {
+           
+                Console.WriteLine(name);
+            
+        }
+        Console.ReadLine();
+
+    }
+}
+
