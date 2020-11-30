adb034abc4fbeb1eba308bb73d8590159b687528
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-11-30 14:59:44 -0700

    ClassMethodAssignemnt

---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassMethodAssignment;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //calling and setting the class
+        UserMath math = new UserMath();
+        //taking user input
+        Console.WriteLine("Please enter a number to do some math on: ");
+        int userNum = Convert.ToInt32(Console.ReadLine());
+
+        //calling the method in the class
+        int equal = math.Addition(userNum);
+        Console.WriteLine(userNum + " + 5 = " + equal);
+
+        //calling the method in the class
+        int equal2 = math.Subtraction(userNum);
+        Console.WriteLine(userNum + " - 5 = " + equal2);
+
+        //calling the method in the class
+        int equal3 = math.Multi(userNum);
+        Console.WriteLine(userNum + " * 5 = " + equal3);
+
+
+        Console.ReadLine();
+    }
+}
+
---
+++ ClassMethodAssignment/ClassMethodAssignment/UserMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    public class UserMath
+    {
+        public UserMath()
+        {
+
+        }
+
+        public int Addition(int userNum)
+        {
+            
+            int operation1 = userNum + 5;
+            return operation1;
+
+        }
+
+        public int Subtraction(int userNum)
+        {
+
+            int operation1 = userNum - 5;
+            return operation1;
+
+        }
+
+        public int Multi(int userNum)
+        {
+
+            int operation1 = userNum * 5;
+            return operation1;
+
+        }
+    }
+}
