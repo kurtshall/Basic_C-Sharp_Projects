c09111d564dc2fda0230f2c610274e5b1bc66b8b
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-12-02 12:23:15 -0700

    MethodAssignment2

---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            classMethod classMethod = new classMethod();
+
+
+            Console.WriteLine("Please enter a number to do addition on: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            int equal = classMethod.addition(userNum);
+            Console.WriteLine(userNum + " + 12 = " + equal);
+
+
+            Console.WriteLine("Please enter a number to do multiplication on: ");
+            double userNum2 = Convert.ToDouble(Console.ReadLine());
+
+            int equal2 = classMethod.multi(userNum2);
+            Console.WriteLine(userNum2 + " * 4 = " + equal2);
+
+
+            Console.WriteLine("Please enter a number to do subtraction on: ");
+            string userNum3 = Console.ReadLine();
+
+            int equal3 = classMethod.subtract(userNum3);
+            Console.WriteLine(userNum3 + " - 2 = " + equal3);
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/classMethod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class classMethod
+    {
+        public classMethod()
+        {
+
+        }
+
+        public int addition(int userNum)
+        {
+            Console.WriteLine("You entered the number: " + userNum);
+            int total = userNum + 12;
+            return total;
+        }
+
+        public int multi(double userNum2)
+        {
+            Console.WriteLine("You entered the number: " + userNum2);
+            int total = Convert.ToInt32(userNum2 * 4);
+            return total;
+        }
+
+        public int subtract(string userNum3)
+        {
+            Console.WriteLine("You entered the number: " + userNum3);
+            int total = Convert.ToInt32(userNum3) - 2;
+            return total;
+        }
+
+
+    }
+}
