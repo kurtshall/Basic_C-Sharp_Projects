9729061d40a9f02f416a4bca92407a036c8000d3
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-12-02 13:14:02 -0700

    Method2Assignment

---
+++ Method2Numbers/Method2Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2Numbers
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            classOne classOne = new classOne();
+            Console.WriteLine("Please enter one number and a optional second number.");
+            Console.WriteLine("Number one: ");
+            int userNum2;
+            int result;
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Optional number two: ");
+            if (int.TryParse(Console.ReadLine(), out userNum2))
+                result = classOne.addition(userNum1, userNum2);
+            else
+                result = classOne.addition(userNum1);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Method2Numbers/Method2Numbers/classOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2Numbers
+{
+    public class classOne
+    {
+        public classOne()
+        {
+
+        }
+
+        public int addition(int userNum1, int userNum2 = 3)
+        {
+            Console.WriteLine("The first number is: " + userNum1 + " and the optional second number is: " + userNum2);
+            int total = userNum1 + 7 + userNum2;
+            Console.WriteLine(userNum1 + " + 7 + " + userNum2 + " = " + total);
+            return total;
+
+        }
+    }
+}
