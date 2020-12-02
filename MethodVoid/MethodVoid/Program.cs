8f5889a16337414f8feeed28ebf9c6603bbd7573
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-12-02 14:03:56 -0700

    VoidMethod Assignment

---
+++ MethodVoid/MethodVoid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodVoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            voidClass voidClass = new voidClass();
+
+            Console.WriteLine("Please enter two numbers into the system.");
+            Console.WriteLine("Number one: ");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number two: ");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+
+            voidClass.oper(numOne, numTwo);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodVoid/MethodVoid/voidClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodVoid
+{
+    public class voidClass
+    {
+        public voidClass()
+        {
+
+        }
+
+        public void oper(int numOne, int numTwo)
+        {
+            Console.WriteLine("You entered for number one: " + numOne + " and for number two: " + numTwo);
+            int total = numOne + 7;
+            Console.WriteLine(numOne + " + 7 = " + total);
+            Console.WriteLine(numTwo + " is your second number!");
+
+        }
+    }
+}
