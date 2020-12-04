5bc58e86dba25209934211ab9a5b8b4b439d244f
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-12-04 14:14:42 -0700

    Inherit Assignment

---
+++ InheritAssignment/InheritAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is an inherit project.");
+
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
