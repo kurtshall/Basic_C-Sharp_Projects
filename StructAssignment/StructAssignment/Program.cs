aa0ed05999f76bd275f5373b06125d50802e7c4f
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-12-07 14:51:00 -0700

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 3.45M;
+
+            Console.WriteLine("This will print out a decimal:");
+            Console.WriteLine(number.Amount);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
