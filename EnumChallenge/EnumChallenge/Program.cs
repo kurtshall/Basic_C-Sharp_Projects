bed1d877e03a51fa8f6bd9ab104a8f1f57ce0b37
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-12-07 14:03:51 -0700

    Enum Assignment

---
+++ EnumChallenge/EnumChallenge/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumChallenge
+{
+    class Program
+    {
+        public enum Week
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday,
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week: ");
+            string userDay = Console.ReadLine();
+
+            Week day;
+            try
+            {
+                day = (Week)Enum.Parse(typeof(Week), userDay);
+                Console.WriteLine("It worked");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.WriteLine(ex.Message);
+
+                
+            }
+
+            
+
+
+
+
+            Console.ReadLine();
+
+        }
+       
+        
+    }
+}
