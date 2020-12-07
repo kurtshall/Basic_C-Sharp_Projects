5914fe914f405bd3f191f9f11fa3605bbb290d49
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-12-06 21:44:22 -0700

    Generic Loiop

--- GenericPerameter/GenericPerameter/Program.cs
+++ GenericPerameter/GenericPerameter/Program.cs
@@ -12,13 +12,29 @@ namespace GenericPerameter
         {
             Employee<string> employee = new Employee<string>();
 
-            employee.things = new List<string>() ;
+            employee.things = new List<string>() { "Hello", "World", "Earth" };
 
 
 
             Employee<int> employee2 = new Employee<int>();
 
-            employee2.things = new List<int>();
+            employee2.things = new List<int>() { 4, 5, 23, 128 };
+
+
+            foreach (string word in employee.things)
+            {
+                Console.WriteLine(word);
+            }
+
+            foreach (int g in employee2.things)
+            {
+                Console.WriteLine(g);
+            }
+
+
+            
+
+            Console.ReadLine();
             
 
 
