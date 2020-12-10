92882bbad3934909d19052612e4d99589a1d2e86
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-12-10 14:15:12 -0700

    TimeAssignment

---
+++ DateTimeAssignemnt/DateTimeAssignemnt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignemnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Enter a number: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            DateTime today = DateTime.Now;
+            DateTime total = today.AddHours(userNum);
+            Console.WriteLine(total);
+
+            
+           
+
+            Console.ReadLine();
+        }
+    }
+}
