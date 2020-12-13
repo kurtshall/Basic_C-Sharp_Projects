4ed960d1e9c3dc6b3015c8c9575faf7384e8019d
kurtshall <70974074+kurtshall@users.noreply.github.com>
2020-12-13 13:57:29 -0700

    Exception Assignemt

---
+++ ASSIGNMENTS/ExceptionHandlesAssignment/ExceptionHandlesAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ExceptionHandlesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                int year = DateTime.Now.Year;
+                if (age < 1)
+                {
+                    throw new ArgumentException();
+                }
+
+                Console.WriteLine("You were born in " + (year - age) + "!");
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Enter a age that is not negative or 0: ");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong");
+                Console.ReadLine();
+                return;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
